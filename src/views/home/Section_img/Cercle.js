98501494b0dbbe8213ab589bdd3cde7761f4a6ae
mamihasina-sarobidy <31620381+mamihasina-sarobidy@users.noreply.github.com>
2022-05-10 23:09:36 +0900

    sary
    change sary

--- src/views/home/Section_img/Cercle.js
+++ src/views/home/Section_img/Cercle.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
-import sary2 from './../../../img/sar1.jpg';
+//import sary2 from './../../../img/sar1.jpg';
+import sary2 from './../../../img/a4.jpg';
 import sary3 from './../../../img/a2.jpg';
 import sary1 from './../../../img/f1.jpg';
 import sary4 from './../../../img/a3.jpg';
